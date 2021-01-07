0d6a763e0ce5b33355336f6e135b7aa54c6c4291
Tudo <tudoracherobert96@yahoo.com>
2021-01-07 08:58:25 +0200

    ProfiCircle first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState, useEffect} from 'react'
+import IntTest from './Components/IntTest';
 
 function App() {
+    const [userInfos,setUserInfos] = useState([]);
+    useEffect(() => {
+        console.log(userInfos);
+    },[userInfos]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{userInfos.length}</h1>
+      <IntTest userInfos={userInfos} onChangeUserInfos={(newUserInfos) => setUserInfos(newUserInfos)}/>
     </div>
   );
 }
